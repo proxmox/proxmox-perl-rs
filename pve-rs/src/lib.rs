b905cfd03d41a6e00c54744afc81c2c8deb8ea70
Lukas Wagner <l.wagner@proxmox.com>
2023-11-14 13:59:26 +0100

    pve-rs: notify: remove notify_context for PVE
    The context has now been moved to `proxmox-notify` due to the fact
    that we also need it in `proxmox-mail-forward` now.
    
    Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

--- pve-rs/src/lib.rs
+++ pve-rs/src/lib.rs
@@ -4,18 +4,19 @@
 pub mod common;
 
 pub mod apt;
-pub mod notify_context;
 pub mod openid;
 pub mod resource_scheduling;
 pub mod tfa;
 
 #[perlmod::package(name = "Proxmox::Lib::PVE", lib = "pve_rs")]
 mod export {
-    use crate::{common, notify_context};
+    use proxmox_notify::context::pve::PVE_CONTEXT;
+
+    use crate::common;
 
     #[export]
     pub fn init() {
         common::logger::init("PVE_LOG", "info");
-        notify_context::init();
+        proxmox_notify::context::set_context(&PVE_CONTEXT)
     }
 }
