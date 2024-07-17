885830935c1cd17bfa22732c2350ce1d23c11030
Wolfgang Bumiller <w.bumiller@proxmox.com>
2024-07-17 12:44:46 +0200

    update to sys 0.6 and proxmox-log crate
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- common/src/logger.rs
+++ common/src/logger.rs
@@ -1,14 +1,14 @@
-use env_logger::{Builder, Env};
-use std::io::Write;
+use anyhow::Error;
 
 /// Initialize logging. Should only be called once
 pub fn init(env_var_name: &str, default_log_level: &str) {
-    if let Err(e) = Builder::from_env(Env::new().filter_or(env_var_name, default_log_level))
-        .format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()))
-        .write_style(env_logger::WriteStyle::Never)
-        .format_timestamp(None)
-        .try_init()
+    if let Err(e) = default_log_level
+        .parse()
+        .map_err(Error::from)
+        .and_then(|default_log_level| {
+            proxmox_log::init_logger(env_var_name, default_log_level, "<pve>")
+        })
     {
-        eprintln!("could not set up env_logger: {e}");
+        eprintln!("could not set up env_logger: {e:?}");
     }
 }
