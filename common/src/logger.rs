9a91594ee61d7f0bf7987c23320e998bb38afb9b
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2024-07-25 12:56:06 +0200

    update to proxmox-log 0.2
    Reviewed-by: Lukas Wagner <l.wagner@proxmox.com>
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- common/src/logger.rs
+++ common/src/logger.rs
@@ -5,9 +5,7 @@ pub fn init(env_var_name: &str, default_log_level: &str) {
     if let Err(e) = default_log_level
         .parse()
         .map_err(Error::from)
-        .and_then(|default_log_level| {
-            proxmox_log::init_logger(env_var_name, default_log_level, "<pve>")
-        })
+        .and_then(|default_log_level| proxmox_log::init_logger(env_var_name, default_log_level))
     {
         eprintln!("could not set up env_logger: {e:?}");
     }
